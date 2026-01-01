cce3a6d18861aec0f0e4742ed85b83a447228d40
Wilton de Paula Soares Junior <camera3.tuca@gmail.com>
2026-01-01 16:02:09 -0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,25 @@
+"""
+Agro Monitor Pro v2.0
+Sistema Profissional de Monitoramento do Agronegócio
+"""
 
+__version__ = "2.0.0"
+__author__ = "Agro Monitor Team"
+
+from .database import AgroDatabase
+from .technical_analysis import TechnicalAnalysisEngine
+from .fundamental_analysis import FundamentalAnalysisEngine
+from .news_analysis import NewsAnalysisEngine
+from .monitoring_system import AgroMonitoringSystem
+from .cache_manager import CacheManager
+from .scheduler import SchedulerManager
+
+__all__ = [
+    'AgroDatabase',
+    'TechnicalAnalysisEngine',
+    'FundamentalAnalysisEngine',
+    'NewsAnalysisEngine',
+    'AgroMonitoringSystem',
+    'CacheManager',
+    'SchedulerManager'
+]
